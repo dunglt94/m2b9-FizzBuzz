44947a6a3a606ec2f73daef79c72d57bb72e23d9
dunglt94 <tuandung9594@gmail.com>
2024-10-22 00:11:20 +0700

    pass case o nhap vao co chua so 3 the ket qua la Fizz

--- Test/FizzBuzzTest.java
+++ Test/FizzBuzzTest.java
@@ -8,7 +8,7 @@ public class FizzBuzzTest {
     @DisplayName("So chia het cho 3 thi chuoi la Fizz")
     public void TestGetFizzBuzzCaseDividedBy3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        int number = 3;
+        int number = 12;
 
         String result = "Fizz";
         String actual = fizzBuzz.getFizzBuzz(number);
@@ -19,7 +19,7 @@ public class FizzBuzzTest {
     @DisplayName("So chia het cho 5 thi chuoi la Buzz")
     public void TestGetFizzBuzzCaseDividedBy5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        int number = 5;
+        int number = 25;
 
         String result = "Buzz";
         String actual = fizzBuzz.getFizzBuzz(number);
@@ -30,9 +30,20 @@ public class FizzBuzzTest {
     @DisplayName("So chia het cho 3 va 5 thi chuoi la FizzBuzz")
     public void TestGetFizzBuzzCaseDividedBy3And5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        int number = 15;
+        int number = 105;
 
         String result = "FizzBuzz";
+        String actual = fizzBuzz.getFizzBuzz(number);
+        assertEquals(result, actual);
+    }
+
+    @Test
+    @DisplayName("So nhap vao co chua so 3 the ket qua la Fizz")
+    public void TestGetFizzBuzzCaseNumberContain3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int number = 13;
+
+        String result = "Fizz";
         String actual = fizzBuzz.getFizzBuzz(number);
         assertEquals(result, actual);
     }
--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,16 +1,48 @@
 public class FizzBuzz {
     public String getFizzBuzz(int number) {
         String result = "";
-        if (number % 3 == 0) {
-            if (number % 5 == 0) {
+        boolean isDividedBy3 = number % 3 == 0;
+        boolean isDividedBy5 = number % 5 == 0;
+
+        String numberString;
+        boolean contains3 = String.valueOf(number).contains("3");
+        boolean contains5 = String.valueOf(number).contains("5");
+        if (isDividedBy3 && isDividedBy5 || contains3 && contains5) {
                 result = "FizzBuzz";
-            } else {
-                result = "Fizz";
-            }
-        } else if (number % 5 == 0) {
-            result = "Buzz";
+        } else if (isDividedBy3 || contains3) {
+                    result = "Fizz";
+        } else if (isDividedBy5 || String.valueOf(number).contains("5")) {
+                result = "Buzz";
         }
+
 
         return result;
     }
+
+//    public String getWordFromNumber(char number) {
+//        switch (number) {
+//            case '0':
+//                return "muoi";
+//            case "1":
+//                return "mot";
+//            case "2":
+//                return "hai";
+//            case "3":
+//                return "ba";
+//            case "4":
+//                return "bon";
+//            case "5":
+//                return "nam";
+//            case "6":
+//                return "sau";
+//            case "7":
+//                return "bay";
+//            case "8":
+//                return "tam";
+//            case "9":
+//                return"chin";
+//            default:
+//                return"";
+//        }
+//    }
 }
